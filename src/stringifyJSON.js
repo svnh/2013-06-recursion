df9c8940381512a0112a607287cfe830f63367a5
sekunovsky <sekunovsky@gmail.com>
2013-06-14 03:06:38 -0700

    returns values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,38 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var res = [];
+	
+	switch (typeof obj) {
+		case 'number' :
+			return String(obj);
+		case 'null' :
+			return String(obj);
+		case 'boolean' :
+			return String(obj);
+		case 'string' :
+			return "\"" + obj + "\"";
+		case 'object' : 
+			if (!obj) {
+				return 'null';
+			}
+			if (Object.prototype.toString.apply(obj) === '[object Array]') {
+                for (var i = 0; i < obj.length; i += 1) {
+                    res[i] = (stringifyJSON(obj[i]) || 'null');
+                }
+                return "[" + res + "]";
+            }
+            else {
+            	for (var j in obj) {
+            		if (Object.prototype.hasOwnProperty.call(obj, j)) {
+            			var key = stringifyJSON(j);
+            			var value = stringifyJSON(obj[j])
+            			if (value) {
+            				res.push(key + ":" + value);
+            			}
+            		}
+            	}
+            }	
+			return "{" + res + "}";
+     }
 };
